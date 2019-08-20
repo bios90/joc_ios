fac039cc9855e4120d5179b3bc1c0afaf03a4573
bios90 <bios90@mail.ru>
2019-08-20 14:43:51 +0300

    Initial Commit

---
+++ Joc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Joc
+//
+//  Created by Филипп Бесядовский on 20/08/2019.
+//  Copyright © 2019 dimfcompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
